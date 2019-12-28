b1cc18904db7989da073b17e6eec8ac5ef6a04f8
vmanriquec <pool_rodrigo1@yahoo.com>
2019-12-27 23:50:46 -0500

    copia total

---
+++ app/src/main/java/com/sodapop/sodafood/modelo/Productosporfamilia.java
@@ -0,0 +1,13 @@
+
+package com.sodapop.sodafood.modelo;
+
+/**
+ * Created by Perseo on 06/08/2014.
+ */
+public class Productosporfamilia {
+    private int idproducto;
+    private String nombreproducto;
+    private Double precventa;
+
+
+}
