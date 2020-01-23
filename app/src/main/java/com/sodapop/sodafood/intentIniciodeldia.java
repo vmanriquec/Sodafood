5db1637610723e0e01b65a617d81627b630194d1
vmanriquec <pool_rodrigo1@yahoo.com>
2020-01-23 14:24:41 -0500

    layoutinicio

--- app/src/main/java/com/sodapop/sodafood/intentIniciodeldia.java
+++ app/src/main/java/com/sodapop/sodafood/intentIniciodeldia.java
@@ -3,6 +3,10 @@ package com.sodapop.sodafood;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class intentIniciodeldia extends AppCompatActivity {
 
@@ -10,5 +14,9 @@ public class intentIniciodeldia extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.controlincial);
+       TextView fechadehoy =(TextView)findViewById(R.id.fechaactual);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        String currentDateandTime = sdf.format(new Date());
+        fechadehoy.setText(currentDateandTime);
     }
 }
